9bd5f903a3e43bf4f9cd8297406d623f18604b63
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2013-08-08 18:02:09 +0900

    変数名修正

--- test_score.rb
+++ test_score.rb
@@ -18,39 +18,39 @@ describe Score do
     end
 
     it "kenの最後のスコアは３００" do
-      @ken.display_totalscore[9].should == 300
+      @ken.totalscores[9].should == 300
     end
 
     it "taroの最後のスコアは112" do
-      @taro.display_totalscore[9].should == 112
+      @taro.totalscores[9].should == 112
     end
 
     it "taroの１フレーム目のスコアは6" do
-      @taro.display_totalscore[0].should == 6
+      @taro.totalscores[0].should == 6
     end
     it "taroの２フレーム目のスコアは11" do
-      @taro.display_totalscore[1].should == 11
+      @taro.totalscores[1].should == 11
     end
     it "taroの3フレーム目のスコアは31" do
-      @taro.display_totalscore[2].should == 31
+      @taro.totalscores[2].should == 31
     end
     it "taroの4フレーム目のスコアは48" do
-      @taro.display_totalscore[3].should == 48
+      @taro.totalscores[3].should == 48
     end
     it "taroの5フレーム目のスコアは55" do
-      @taro.display_totalscore[4].should == 55
+      @taro.totalscores[4].should == 55
     end
     it "taroの6フレーム目のスコアは65" do
-      @taro.display_totalscore[5].should == 65
+      @taro.totalscores[5].should == 65
     end
     it "taroの7フレーム目のスコアは65" do
-      @taro.display_totalscore[6].should == 65
+      @taro.totalscores[6].should == 65
     end
     it "taroの8フレーム目のスコアは90" do
-      @taro.display_totalscore[7].should == 90
+      @taro.totalscores[7].should == 90
     end
     it "taroの9フレーム目のスコアは106" do
-      @taro.display_totalscore[8].should == 106
+      @taro.totalscores[8].should == 106
     end
 
     after do
